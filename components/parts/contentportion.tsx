f7183dd370d3166183d75cad77845292ca2b4986
Spsden <pratapsinghsuraj420@gmail.com>
2022-11-29 16:54:46 +0530

    added titlebar

--- components/parts/contentportion.tsx
+++ components/parts/contentportion.tsx
@@ -1,28 +1,39 @@
-import React from 'react'
-import {Rnd} from "react-rnd";
+import React from "react";
+import { Rnd } from "react-rnd";
+import TitleBar from "./windowManager/titlebar";
 
 function ContentArea() {
   return (
-    <Rnd  style={{
-        backdropFilter: "blur(30px)",
-        margin: "10px",
-        right: "5px",
-        left: "5px",
-        border:'solid black'
-      }}className='lex align-center justify-center border-red-700 '
-    default={
-        {
-            x:0,
-            y:0,
-            width:200,
-            height:200
-        }
-    }>
-         <div className='flex align-center justify-center border-neutral-300 bg-black'>Hello Brother</div>
-
+    <Rnd
+      className="lex align-center justify-center border-red-700 "
+      default={{
+        x: 0,
+        y: 0,
+        width: 200,
+        height: 200,
+      }}
+      dragHandleClassName="dragarea"
+    >
+      <div
+        style={{
+          backdropFilter: "blur(30px)",
+          margin: "10px",
+          right: "5px",
+          left: "5px",
+          border: "solid black",
+        }}
+        className="flex flex-col border-neutral-300 h-full w-full"
+      >
+        <TitleBar />
+        <div className="h-full w-full">
+          {" "}
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printeitnc
+        </div>
+      </div>
     </Rnd>
-   
-  )
+  );
 }
 
-export default ContentArea
+export default ContentArea;
---
+++ components/parts/windowManager/titlebar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+function TitleBar() {
+  return (
+    <div
+      style={{
+        backdropFilter: "blur(50px)",
+      }}
+      className="dragarea  w-full h-8 flex mb-5 justify-between  pl-2 bg-zinc-900   "
+    >
+      <div>
+        <h3>Drip Player</h3>
+      </div>
+      <div className="">
+        <button className="w-4 m-2 hover:bg-slate-700  align-middle">
+          <img src="/assets/minimize.png" />
+        </button>
+        <button className="w-4 m-2  align-middle">
+          <img src="/assets/maximize.png" />
+        </button>
+        <button className="w-4 m-2  align-middle">
+          <img src="/assets/close.png" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default TitleBar;
